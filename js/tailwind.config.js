c4a6e11547a5cc5b6ed4f10ef6ae0787d634b00a
Ghislano26 <ghislainatebachouacha@gmail.com>
2024-03-06 14:10:31 +0100

    mobilefirst close

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      min: 'var(--color-min)',
+
+    }
+  }
+}
