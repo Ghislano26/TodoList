514dd7f37f1f227ef013eeea9cd5729b2efa2ef5
Ghislano26 <ghislainatebachouacha@gmail.com>
2024-03-16 10:53:07 +0100

    js action

--- js/index.js
+++ js/index.js
@@ -1,71 +1,78 @@
-const checkImg = document.querySelector('#checkImg');
 const inputList = document.querySelector('#inputList');
+const checkImg = document.querySelector('#checkImg');
 const circle_hover = document.querySelectorAll('#circle_hover');
-const light_mode_img = document.querySelector('#light_mode_img');
-const section = document.querySelector('section');
-const todolist = document.querySelector('#todolist');
-const inputTodo = document.querySelector('#inputTodo');
-const h3 = document.querySelectorAll('h3');
+const block = document.querySelector('#todolist');
+let isClicked = false;
 
 
 
-// Function pour changer d'image apres 5 caracteres entres par utialisateurs
-function verifChar() {
+
+function verifChar(){
     checkImg.src = 'assets/img/Oval.svg';
-    if (inputList.value.length >= 5) {
-        checkImg.src = 'assets/img/check-active.svg';
+    if (inputList.value.length >=5) {
+        checkImg.src = 'assets/img/check-active.svg'; 
     }
     else{
         checkImg.src = 'assets/img/Oval.svg';
     }
-}
+} 
 
-// Effects sur les boutton de listes 
-circle_hover.forEach(element => {
-    element.addEventListener('mouseover', ()=>{
-        element.src = '/assets/img/check-circle-hover.svg';
+// circle_hover.forEach(a =>{
+//     a.addEventListener('mouseover', ()=>{
+//         a.src = 'assets/img/check-circle-hover.svg';
+//     })
+// })
+
+// circle_hover.forEach(a =>{
+//     a.addEventListener('mouseout', ()=>{
+//         a.src = 'assets/img/Oval.svg';
+//     })
+// })
+
+circle_hover.forEach(a =>{
+    a.addEventListener('click', ()=>{
+        isClicked ? (a.src = 'assets/img/check-active.svg') : (a.src = 'assets/img/Oval.svg');
+        isClicked = !isClicked;
     })
-    element.addEventListener('mouseout', ()=>{
-        element.src = '/assets/img/Oval.svg';
-    })
-});
-
-// Dark mode training
-light_mode_img.addEventListener('click', ()=>{
-    if (light_mode_img.src = '/assets/img/img-light.svg') {
-        light_mode_img.src = '/assets/img/img-night.svg';
-
-        section.style.backgroundImage = "url('assets/img/nignt-mode.svg')";
-        section.style.backgroundSize = "cover";
-        section.style.backgroundPosition = "center";
-        section.style.backgroundRepeat = "no-repeat";
-
-
-        document.body.style.background = '#171823';
-        inputList.style.background = '#25273D';
-        inputTodo.style.background = '#25273D';
-        todolist.style.background = '#25273D';
-        todolist.style.boxShadow = '0px 0px 0px 0px black';
-        h3.forEach(x =>{
-            x.style.color = 'white';
-            x.style.fontSize = '1.2rem';
-        })
-
-    }
-    
-   
 })
 
 
 
+function addTodo(valeur) {
+        const text = `
+        <div class="w-full h-[18%] flex items-center gap-[1rem] pl-[1rem] border-b-[1.5px] border-[var(color-bottom)] font-semibold hover:bg-slate-200">
+        <div class="flex  flex-[2] gap-[1rem]">
+            <img src="assets/img/Oval.svg" alt=""  id="circle_hover" class="cursor-pointer sm:h-[2rem] md:h-[2.5rem]">
+            <h3 class="text-[1.2rem] font sm:text-[2rem] md:text-[2.5rem] 2xl:text-[1.2rem] text_one">${valeur}</h3>
+        </div>
+        <div class="flex items-center justify-end flex-1 text-[2rem] text-gray-400 ">
+            <ion-icon name="close-outline" class="croix sm:text-[2rem] md:text-[3rem] xl:text-[4rem] 2xl:text-[2.5rem]"></ion-icon>
+        </div>
+        </div>
+        `;
+        const position = 'afterbegin';
+        block.insertAdjacentHTML(position, text);
 
-// ternaire
-// let isClicked = false;
-// condition ? NON : OUI
+        // const pos = 'beforeend';
+        // task.insertAdjacentHTML(pos,word);
+}
+document.addEventListener('keyup', (e)=>{
+    if (e.keyCode == 13) {
+        const valeur = inputList.value;
+        if (valeur.length <= 5) return;
+        addTodo(valeur);
+        inputList.value = " ";
+       
+    
+    }
+    
+})
 
-// function tag(id){
-//     document.getElementById(id);
-//     if (!isClicked) {
-        
+// document.addEventListener('keyup', (event) =>{
+//     if (event.keyCode == 13) {
+//         const input = inputList.value;
+//         if (input.length <= 5) return;
+//         add(input);
+//         inputList.value = "";
 //     }
-// }
+// })
